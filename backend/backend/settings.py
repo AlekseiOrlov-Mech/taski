606e361929e6c62502345bdb52b0daea058c2a0b
Aleksei Orlov <python.masster2000@gmail.com>
2026-06-12 12:15:53 +0600

    change domain

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123', 'kittygrammmmmmm.adrdns.com']
+ALLOWED_HOSTS = ['localhost', '123.123.123.123', 'trainexample.servemp3.com']
 
 
 # Application definition
